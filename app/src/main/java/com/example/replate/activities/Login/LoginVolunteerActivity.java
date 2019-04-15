9000a08471324362b50ca90c54887e9503b3ce47
Spencer Stock <spencerstock@gmail.com>
2019-04-15 11:38:42 -0600

    added basic login activities

---
+++ app/src/main/java/com/example/replate/activities/Login/LoginVolunteerActivity.java
@@ -0,0 +1,15 @@
+package com.example.replate.activities.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.replate.R;
+
+public class LoginVolunteerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_volunteer);
+    }
+}
